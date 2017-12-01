5735f91e512ff85aac229f08b515a9b9f68bc79f
Tinia Liu <liutianyipku@gmail.com>
2017-11-30 22:57:32 -0500

    Float vectors for glove json

--- glove_parse/glove_parse.py
+++ glove_parse/glove_parse.py
@@ -11,13 +11,13 @@ with open('glove.6B.50d.txt', 'r') as glove:
 		vec = line.strip().split(" ")
 		ind = ord(vec[0][0])
 		if ind >= 65 and ind <= 73 or ind >= 97 and ind <= 105:
-			glove_dict_A_I[vec[0]] = vec[1:]
+			glove_dict_A_I[vec[0]] = [float(_) for _ in vec[1:]]
 		elif ind >= 74 and ind <= 83 or ind >= 106 and ind <= 115:
-			glove_dict_J_S[vec[0]] = vec[1:]
+			glove_dict_J_S[vec[0]] = [float(_) for _ in vec[1:]]
 		elif ind >= 84 and ind <= 96 or ind >= 116 and ind <= 122:
-			glove_dict_T_Z[vec[0]] = vec[1:]
+			glove_dict_T_Z[vec[0]] = [float(_) for _ in vec[1:]]
 		else:
-			glove_else[vec[0]] = vec[1:]
+			glove_else[vec[0]] = [float(_) for _ in vec[1:]]
 
 
 with open('glove_dict_A_I.json', 'w') as f:
